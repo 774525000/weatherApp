54b00765fd8cff219f9e75dfcffb414ff0b1280c
hehe <774525000@qq.com>
2021-05-12 18:06:01 +0800

    ❤❤❤❤❤

--- app/src/main/java/com/weather/app/ui/viewModel/MainViewModel.kt
+++ app/src/main/java/com/weather/app/ui/viewModel/MainViewModel.kt
@@ -3,13 +3,14 @@ package com.weather.app.ui.viewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
 import com.weather.app.logic.network.api.CityWeatherApi
 
 class MainViewModel : ViewModel() {
 
     private val city = MutableLiveData<String>()
 
-    val weatherModel = Transformations.switchMap(city) {
+    val weatherModel = city.switchMap {
         CityWeatherApi.get(it)
     }
 
