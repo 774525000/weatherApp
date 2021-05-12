fbcc66e77a5373c74c926f9b4e43ef2089825c29
774525000@qq.com <774525000@qq.com>
2021-05-12 20:23:43 +0800

    😎😎😎😎😎

--- app/src/main/java/com/weather/app/ui/activity/MainActivity.kt
+++ app/src/main/java/com/weather/app/ui/activity/MainActivity.kt
@@ -9,6 +9,7 @@ import com.weather.app.ui.viewModel.MainViewModel
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val model: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
